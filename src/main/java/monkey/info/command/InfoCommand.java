7bb556d3c218f79c6c941caf3b7ff7660d7038d2
a3510377 <a102009102009@gmail.com>
2023-11-03 20:34:37 +0800

    fix code style

--- src/main/java/monkey/info/command/InfoCommand.java
+++ src/main/java/monkey/info/command/InfoCommand.java
@@ -4,9 +4,6 @@ import carpet.helpers.TickSpeed;
 import carpet.utils.Messenger;
 import com.mojang.brigadier.CommandDispatcher;
 import net.fabricmc.api.ModInitializer;
-
-import static net.minecraft.server.command.CommandManager.*;
-
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
@@ -16,35 +13,37 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Locale;
 
+import static net.minecraft.server.command.CommandManager.literal;
+
 public class InfoCommand implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("infocommand");
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
-	}
+    public static double getMSPT(MinecraftServer server) {
+        return MathHelper.average(server.lastTickLengths) * 1.0E-6D;
+    }
 
-	public static double getMSPT(MinecraftServer server) {
-		return MathHelper.average(server.lastTickLengths) * 1.0E-6D;
-	}
+    public static double getTPS(double MSPT) {
+        return 1000.0D / Math.max(TickSpeed.time_warp_start_time != 0 ? 0.0 : TickSpeed.mspt, MSPT);
+    }
 
-	public static double getTPS(double MSPT) {
-		return 1000.0D / Math.max(TickSpeed.time_warp_start_time != 0 ? 0.0 : TickSpeed.mspt, MSPT);
-	}
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandManager.RegistrationEnvironment environment) {
+        dispatcher.register(
+                literal("tps").executes((c) -> {
+                    double MSPT = getMSPT(c.getSource().getServer());
+                    String color = Messenger.heatmap_color(MSPT, TickSpeed.mspt);
 
-	public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandManager.RegistrationEnvironment environment){
-		dispatcher.register(
-			literal("tps").executes((c) -> {
-				double MSPT = getMSPT(c.getSource().getServer());
-				String color = Messenger.heatmap_color(MSPT,TickSpeed.mspt);
+                    Messenger.m(
+                            c.getSource(),
+                            "g TPS: ", String.format(Locale.US, "%s %.1f", color, getTPS(MSPT)),
+                            "g  MSPT: ", String.format(Locale.US, "%s %.1f", color, MSPT)
+                    );
+                    return (int) TickSpeed.tickrate;
+                })
+        );
+    }
 
-				Messenger.m(
-					c.getSource(),
-					"g TPS: ", String.format(Locale.US, "%s %.1f", color, getTPS(MSPT)),
-					"g  MSPT: ", String.format(Locale.US,"%s %.1f", color, MSPT)
-				);
-				return (int) TickSpeed.tickrate;
-			})
-		);
-	}
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+    }
 }
