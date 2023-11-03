9a0c150c7af311fb9b9734b732cf94b4f6b0d6de
a3510377 <a102009102009@gmail.com>
2023-11-03 20:32:21 +0800

    init

---
+++ src/main/java/monkey/info/command/mixin/InfoCommandMix.java
@@ -0,0 +1,24 @@
+package monkey.info.command.mixin;
+
+import com.mojang.brigadier.CommandDispatcher;
+import monkey.info.command.InfoCommand;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CommandManager.class)
+public class InfoCommandMix {
+	@Final
+	@Shadow
+	private CommandDispatcher<ServerCommandSource> dispatcher;
+
+	@Inject(method = "<init>", at = @At("RETURN"))
+	private void onRegister(CommandManager.RegistrationEnvironment arg, CallbackInfo ci) {
+		InfoCommand.register(this.dispatcher, arg);
+	}
+}
